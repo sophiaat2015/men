c2c80a71ea5d46df54fec4338dc5f2064511ac19
wangzilong <wangzilong@nigc.com>
2015-06-23 16:54:40 +0800

    add test page

---
+++ modules/tomcat.js
@@ -0,0 +1,12 @@
+var tomcat = {};
+
+tomcat.IP = '121.40.236.187';
+
+tomcat.port = 8888;
+
+tomcat.getUrl = function(){
+    var url = 'http://' + this.IP + ':' + this.port ;
+    return url;
+}
+
+module.exports = tomcat;
---
+++ routes/test.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+var tomcat = require('../modules/tomcat.js');
+
+/* whole path looks like: /test/user/select/id */
+router.get('/', function(req, res, next) {
+  res.render('test', { title: '智慧出行管家Test Page' });
+});
+
+module.exports = router;
