24b9903b82bf3a523248dcda963a815860a997a7
wangzilong <wangzilong@nigc.com>
2015-06-24 14:20:44 +0800

    access to weixin

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var test = require('./routes/test');
+var wechat = require('./routes/wechat');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/test', test);
+app.use('/wechat', wechat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/wechat.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var crypto = require('crypto');
+var url = require('url');
+
+
+function checkSignature(signature,timestamp,nonce,token){
+    var array = new Array();
+    array[0] = timestamp;
+    array[1] = nonce;
+    array[2] = token;
+    array.sort();
+    var hasher = crypto.createHash('sha1');
+    var msg = array[0]+array[1]+array[2];
+    hasher.update(msg);
+    var msg = hasher.digest('hex');  // 计算SHA1值
+    if(msg === signature){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+var config = {
+    token: 'nigcbd'
+};
+
+
+/*  */
+router.get('/', function(req, res, next) {
+    var url_parts = url.parse(request.url, true);
+    var query = url_parts.query;
+
+    var signature = query.signature;
+    var timestamp = query.timestamp;
+    var nonce     = query.nonce;
+    var echostr   = query.echostr;
+
+    var token = config.token;
+
+    if (checkSignature(signature, timestamp, nonce, token)) {
+        res.send(echostr);
+    }else{
+        console.log('微信接入验证失败');
+        res.send(null);
+    }
+});
+
+module.exports = router;
